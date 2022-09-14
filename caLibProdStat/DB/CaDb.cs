b0564be220c0a3fbc7f81d3f0e3da4a46be48a9a
Andrey Mizerov <andrey@mizerov.com>
2022-09-14 15:47:19 +0300

    TrustServerCert

---
+++ caLibProdStat/DB/CaDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace caLibProdStat;
+
+public class CaDb: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=progerx;Database=CryptoAlert;UID=ca;PWD=1qaz!QAZ;TrustServerCertificate=true;");
+    }
+    public DbSet<Product>? Products { get; set; }
+}
